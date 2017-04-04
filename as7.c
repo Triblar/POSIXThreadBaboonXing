106da399337ebd13b58cf23fdd7f72449795e7fe
James Dempsey <jed11@DESKTOP-MVAQ684.(none)>
2017-04-04 13:43:26 -0400

    Removed header file for simplicity

--- as7.c
+++ as7.c
@@ -5,7 +5,30 @@
  * Adapted from the solutions to POSIX Thread problems from http://eecs-002.case.edu/338.S17/old%20assignments%20and%20exams.html
  */
 
-#include <baboon.h>
+ #include <sys/types.h>
+ #include <sys/ipc.h>
+ #include <sys/sem.h>
+ #include <sys/shm.h>
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <sys/wait.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <string.h>
+ #include <pthread.h>
+ #include <semaphore.h>
+
+ #ifndef NUM_THREADS
+ #define NUM_THREADS 200
+ #endif
+
+ #define CROSS_ROPE_STALL_TIME	100000
+
+ extern const unsigned int RAND_RANGE;
+
+ typedef struct _thread_data_t {
+ 	int tid;//thread id
+ } thread_data_t;
 
 const unsigned int RAND_RANGE = RAND_MAX>>10;
 
